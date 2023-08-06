31f8d5268310009e1e587de39781a2b44b20e0df
nokiaisfinland <141440656+nokiaisfinland@users.noreply.github.com>
2023-08-06 21:01:54 +0800

    setup basic bare bone

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue'; // Use createApp instead of importing Vue
+import App from './App.vue';
+import axios from 'axios';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.$http = axios.create({
+    baseURL: 'mongodb://0.0.0.0:27017/mevnonc', // Replace with your backend server URL
+});
+
+app.mount('#app');
